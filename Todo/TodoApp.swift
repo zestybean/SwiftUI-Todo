ee126ef77975a4fb5670928e9301091463f7ab64
Beandip <brando.lugo@gmail.com>
2021-01-23 11:43:03 -0600

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Brando Lugo on 1/23/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
